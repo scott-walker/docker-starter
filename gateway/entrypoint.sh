224e63cfb1e12c6f4fd69932d933c99467a7e3ad
Scott Walker <scott@swlt.ru>
2025-10-04 12:23:40 +0300

    Обновлен Dockerfile и entrypoint.sh для использования шаблона конфигурации nginx. Добавлены переменные окружения для динамической настройки проксирования и SSL-сертификатов в nginx.conf.

--- gateway/entrypoint.sh
+++ gateway/entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 set -e
 
-echo "Starting NGINX"
+echo "Configuring GATEWAY nginx.conf"
+
+sed \
+  -e "s|\$GATEWAY_API_HOST|${GATEWAY_API_HOST}|g" \
+  -e "s|\$GATEWAY_API_PORT|${GATEWAY_API_PORT}|g" \
+  -e "s|\$GATEWAY_API_SERVER_NAME|${GATEWAY_API_SERVER_NAME}|g" \
+  /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+
+echo "Starting GATEWAY"
 nginx -g "daemon off;"
